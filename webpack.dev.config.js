95830b57887a94f0fb99d635e6aafad3b3e1accb
artbie <arturbieszke@gmail.com>
2020-12-23 00:27:45 +0100

    added webpack-dev-server

---
+++ webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main/webapp/javascript/main.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'react-app.js',
+        publicPath: '/dist'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        port: 8080,
+        hot: true,
+        static: path.join(__dirname, 'src/main/resources/static/')
+    }
+};
